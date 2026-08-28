37df0f8cad21220404db8926c8432703f9dc81f7
dylanlegouellec-jpg <dylanlegouellec@gmail.com>
2026-08-28 08:36:17 +0200

    Export shopping components from index.js

---
+++ src/components/shopping/index.js
@@ -0,0 +1,3 @@
+export { default as SwipeFlourish } from "./SwipeFlourish";
+export { default as ShoppingItemRow } from "./ShoppingItemRow";
+export { default as ShoppingView } from "./ShoppingView";
